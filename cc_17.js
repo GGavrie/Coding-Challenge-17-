5a4f4762a11dce41ccc8db77a7cecc1d88997db8
Gavrie Grant <gavrie@usf.edu>
2025-03-30 11:45:25 -0400

    Task 2: Create SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -25,3 +25,29 @@ customer1.addPurchase(250);
 customer1.addPurchase(75);
 
 console.log(`${customer1.name} has spent a total of $${customer1.getTotalSpent()}`);
+
+
+// Task 2: Create a SalesRep Class
+class SalesRep {
+    constructor(name) {
+      this.name = name;
+      this.clients = [];
+    }
+  
+    addClient(customer) {
+      this.clients.push(customer);
+    }
+  
+    getClientTotal(name) {
+      const client = this.clients.find((client) => client.name === name);
+      return client ? client.getTotalSpent() : 0;
+    }
+  }
+  
+  // Testing Task 2
+  console.log("\nTask 2: SalesRep Class");
+  const salesRep1 = new SalesRep("Maria");
+  salesRep1.addClient(customer1);
+  console.log("Sales rep's clients:", salesRep1.clients);
+  console.log(`Total spent by ${customer1.name}: $${salesRep1.getClientTotal(customer1.name)}`);
+  
